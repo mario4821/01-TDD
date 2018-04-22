55da5794958320a98454227172c782d8b133bb3b
Mario Flores Jr <m.flores4821@gmail.com>
2018-04-21 22:04:10 -0700

    added arithmetic portion

--- lab-mario/lib/arithmetic.js
+++ lab-mario/lib/arithmetic.js
@@ -2,31 +2,12 @@
 
 const arithmetic = module.exports = {};
 
-arithmetic = (add) => {
-  if (input === '' || typeof input === 'string') {
-    return null;
-  }
-  else () {
-    return 
-  }
+arithmetic.add = (a, b) => {
+  if (typeof a !== 'number' || typeof b !== 'number') return undefined; 
+  return a + b;
 };
 
-arithmetic.sub = () => {
-  if (input === '' || typeof input === 'string') {
-    return null;
-  }
-  else () {
-    return 
-  }
+arithmetic.sub = (a, b) => {
+  if (typeof a !== 'number' || typeof b !== 'number') return undefined; 
+  return a - b;
 };
-// Create a NodeJS module in the `lib` directory named `arithmetic.js`. This module exports an object and should have `add` and `sub` methods that implement addition and subtraction.
-// * The `add` method should have a 2 parameters (airty of two)
-//   * `if` either argument is a non-number the function should return `null`
-//   * `else` return the sum of the 2 numbers
-// * The `sub` method should have 2 parameters (airty of two)
-//   * `if` either argument is a non-number the function should return `null`
-//   * `else` return the second parameter subtracted from the first parameter
-
-// test each method for proper use (invoked with number arguements)
-
-// test each method for inproper use (invoked with one or more non-number arguments)
--- lab-mario/test/arithmetic.test.js
+++ lab-mario/test/arithmetic.test.js
@@ -3,11 +3,20 @@
 const arithmetic = require('../lib/arithmetic');
 
 describe('Test method for proper use', () => {
-  test('Different ways to use the expect function', () => {
-    const aNullValue = null;
-    expect(aNullValue).toBeNull();
-
-
-// test each method for proper use (invoked with number arguements)
-
-// test each method for inproper use (invoked with one or more non-number arguments)
+  describe('arithmetic.add', () => {
+    test('proper use', () => {
+      expect(arithmetic.add(1, 2)).toEqual(3);
+    });
+    test('non-number test', () => {
+      expect(arithmetic.add()).toBeUndefined();
+    });
+  });
+  describe('arithmetic.sub', () => {
+    test('proper use', () => {
+      expect(arithmetic.sub(8, 3)).toEqual(5);
+    });
+    test('non-number test', () => {
+      expect(arithmetic.sub()).toBeUndefined();
+    });
+  });
+});
